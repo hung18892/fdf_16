4487aad60b1e1d3565375d4b42c4e4bfc3ce45f6
tamnguyen <newuser@example.com>
2016-10-04 16:09:59 +0700

    show index create

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,18 @@ class Product < ApplicationRecord
   has_many :ratings, dependent: :destroy
   has_many :carts_details, dependent: :destroy
   has_many :carts, through: :carts_details
+  validate  :picture_size
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :species, presence: true
+
+  mount_uploader :picture, PictureUploader
+  scope :recent, ->{order created_at: :desc}
+
+  private
+  def picture_size
+    if picture.size > Settings.size_image.megabytes
+      errors.add(:picture, t(:picture_notice))
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get "about" => "static_pages#about"
   get "contact" => "static_pages#contact"
   resources :users
+  resources :products, only: [:create, :show, :destroy, :create, :index, :new]
 end
